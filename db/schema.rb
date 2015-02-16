a12d4fc99487a8c4514b68ea6f7024467bbdcf4d
Andrew Mason <ajm188@case.edu>
2015-02-16 14:33:50 -0500

    removing redundant column on meetings table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150215184523) do
+ActiveRecord::Schema.define(version: 20150216192835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -49,7 +49,6 @@ ActiveRecord::Schema.define(version: 20150215184523) do
     t.integer  "professor_id"
     t.string   "schedule"
     t.string   "room"
-    t.string   "professor"
     t.date     "start_date"
     t.date     "end_date"
     t.datetime "created_at",         null: false
