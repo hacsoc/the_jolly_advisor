da091012f5742fcbae2f3b45d052e77717b72b26
Andrew Mason <mason@case.edu>
2015-03-02 11:08:31 -0500

    move view logic to helper. also link to the prereqs

--- app/helpers/courses_helper.rb
+++ app/helpers/courses_helper.rb
@@ -2,4 +2,17 @@ module CoursesHelper
   def first_professor
     @course.course_instances.first.professor
   end
+
+  def prereq_sets(course)
+    prereq_sets = course.prerequisites
+    if prereq_sets.empty?
+      [content_tag(:li, 'None')]
+    else
+      prereq_sets.map do |prereq_set|
+        content_tag :li do
+          concat prereq_set.map { |prereq| content_tag(:a, prereq.to_s, href: prereq.to_param).to_s }.join(" or ")
+        end
+      end
+    end
+  end
 end
