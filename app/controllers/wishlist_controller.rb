5dbc8e4a9026a5ea9bd478ed5db35bf927b1af1f
Andrew Mason <mason@case.edu>
2015-03-13 23:29:30 -0400

    show user wishlist

--- app/controllers/wishlist_controller.rb
+++ app/controllers/wishlist_controller.rb
@@ -1,6 +1,8 @@
 class WishlistController < ApplicationController
+  before_action :authenticate_user!
 
   # GET /wishlist
   def index
+    @wishlist = current_user.wishlist
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   def to_s
     case_id
   end
+
+  def wishlist
+    WishlistItem.includes(:courses).where(user_id: id).map(&:course)
+  end
 end
