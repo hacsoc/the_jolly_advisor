a5804fa321411211da8b9f237133137c67704bb4
Andrew Mason <mason@case.edu>
2015-03-15 10:34:13 -0400

    add courses to wishlist directly from show page

--- app/controllers/wishlist_controller.rb
+++ app/controllers/wishlist_controller.rb
@@ -5,4 +5,19 @@ class WishlistController < ApplicationController
   def index
     @wishlist = current_user.wishlist
   end
+
+  # POST /wishlist
+  def add_course
+    @wishlist_item = WishlistItem.where(user: current_user, course_id: params[:course_id])
+                                 .first_or_initialize
+    respond_to do |format|
+      if @wishlist_item.save
+        flash[:notice] = 'Course successfully saved to your wishlist'
+        format.html { redirect_to params[:url] || wishlist_path }
+      else
+        flash[:notice] = 'An error occurred adding the course to your wishlist'
+        format.html { redirect_to params[:url] || wishlist_path }
+      end
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
 
   get 'scheduler' => 'scheduler#index'
   get 'wishlist' => 'wishlist#index'
+  post 'wishlist' => 'wishlist#add_course'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
