b8e6a45a43fb6f8a44b9ca30cad01c6e0104c644
Andrew Mason <mason@case.edu>
2015-03-17 15:06:09 -0400

    multining helper methods for readability

--- app/helpers/wishlist_helper.rb
+++ app/helpers/wishlist_helper.rb
@@ -1,9 +1,13 @@
 module WishlistHelper
   def add_remove_link(wishlist_course_ids, course)
-    link_to "#{wishlist_course_ids.include?(course.id) ? 'Remove from' : 'Add to'} my wishlist", wishlist_path(course_id: course.id, url: request.original_url), method: wishlist_course_ids.include?(course.id) ? :delete : :post
+    link_to "#{wishlist_course_ids.include?(course.id) ? 'Remove from' : 'Add to'} my wishlist",
+            wishlist_path(course_id: course.id, url: request.original_url),
+            method: wishlist_course_ids.include?(course.id) ? :delete : :post
   end
 
   def set_notify_link(item)
-    link_to "Turn #{item.notify ? 'off' : 'on'} notifications", wishlist_path(course_id: item.course_id, wishlist_item: { notify: !item.notify }), method: :put
+    link_to "Turn #{item.notify ? 'off' : 'on'} notifications",
+            wishlist_path(course_id: item.course_id, wishlist_item: { notify: !item.notify }),
+            method: :put
   end
 end
