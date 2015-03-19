2cbdd92f27cfe316fb7a31b600855918a92b098d
Andrew Mason <mason@case.edu>
2015-03-19 16:41:04 -0400

    all the comments

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -4,11 +4,20 @@ class Meeting < ActiveRecord::Base
 
   MEETING_TIME_REGEX = /\d{1,2}:\d{1,2} [AP]M/
 
+  # Return an Array of ScheduledMeetings for this Meeting.
+  # Empty if the schedule is TBA
+  # 
+  # All schedules have the form:
+  #   DAY_ABBRVS START_TIME - END_TIME
   def scheduled_meetings
     return [] if schedule == 'TBA'
 
+    # the start time is the first time in the schedule
     start_time = schedule.match(/^.* (#{MEETING_TIME_REGEX}) - #{MEETING_TIME_REGEX}$/).captures.first
+    # the end time is the second time
     end_time = schedule.match(/^.* #{MEETING_TIME_REGEX} - (#{MEETING_TIME_REGEX})$/).captures.first
+    # To get the day abbreviations, only look at the schedule up to the first space.
+    # Then, for each abbreviation, create a ScheduledMeeting for that day
     schedule[0..schedule.index(' ')].scan(/[A-Z][a-z]?/) # MTuWThFSaSu
                                     .map { |day_abbreviation| ScheduledMeeting.new day_abbreviation, start_time, end_time, self }
   end
--- app/models/scheduled_meeting.rb
+++ app/models/scheduled_meeting.rb
@@ -1,6 +1,18 @@
+# For use with the SchedulerController.
+# 
+# CourseInstances have Meetings which have a
+# schedule attribute like 'MWF 10:00 AM - 10:50 AM'
+# 
+# This class represents an actual scheduled meeting
+# for a meeting. So, for the above example schedule,
+# the meeting would have three ScheduledMeetings,
+# one for Monday, one for Wednesday, and one for Friday,
+# each from 10 AM to 10:50 AM.
 class ScheduledMeeting
   attr_reader :meeting, :start_time, :end_time
 
+  # Map the day abbreviations that SIS used to
+  # the symbols used by the Ruby Date/Time classes
   DAY_ABBRV_HASH = {
     'M' => :monday,
     'Tu' => :tuesday,
@@ -11,12 +23,21 @@ class ScheduledMeeting
     'Su' => :sunday
   }
 
+  # The time format that SIS used for class schedules
   TIME_FORMAT = '%l:%M %p'
 
   def initialize(day_abbreviation, start_string, end_string, meeting)
+    # Hold on to the meeting so that we can reference things about it
+    # (course, professor, etc) in the calendar (see app/views/scheduler/index.json.jbuilder)
     @meeting = meeting
 
     day_sym = DAY_ABBRV_HASH[day_abbreviation]
+    # Always jump to the beginning of the week, then advance by the appropriate
+    # number of days. This ensures that @start_time and @end_time are always within
+    # the range of the current week.
+    # Additionally, using DateTime#at_beginning_of_week always sets the time
+    # portion of the DateTime to midnight (that is when the week technically begins after all)
+    # so this guarantee allows the advance function (see below) to work they way it does.
     date = DateTime.now.at_beginning_of_week +
       DateTime::DAYS_INTO_WEEK[day_sym].days
 
