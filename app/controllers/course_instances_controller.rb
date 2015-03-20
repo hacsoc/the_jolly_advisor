9b495d2cfa847bf5f84ce013f55aa5f67336c404
Andrew Mason <mason@case.edu>
2015-03-20 13:31:10 -0400

    update course instance search to only get unenrolled instances

--- app/controllers/course_instances_controller.rb
+++ app/controllers/course_instances_controller.rb
@@ -1,7 +1,7 @@
 class CourseInstancesController < ApplicationController
   # GET /course_instances/autocomplete.json
   def autocomplete
-    @course_instances = CourseInstance.search(params[:term], params[:current_date])
+    @course_instances = CourseInstance.search(params[:term], current_user, params[:current_date])
     respond_to do |format|
       format.json do
         render json: (@course_instances.map do |ci|
--- app/models/course_instance.rb
+++ app/models/course_instance.rb
@@ -7,13 +7,15 @@ class CourseInstance < ActiveRecord::Base
   has_many :enrolled_students, through: :enrollments, source: :user
 
   scope :ongoing, ->(date = Date.today) { where('? BETWEEN course_instances.start_date AND course_instances.end_date', date) }
-  scope :search, ->(term, date = Date.today) {
+  scope :unenrolled, ->(user) { where('NOT EXISTS (SELECT enrollments.id FROM enrollments WHERE enrollments.course_instance_id = course_instances.id AND enrollments.user_id = ?)', user.id) }
+  scope :search, ->(term, user, date = Date.today) {
     includes(:course, :meetings)
     .ongoing(date)
     .joins(:course)
     .where(%{concat(lower(courses.department), courses.course_number) like ?
             OR lower(courses.title) like ?},
             "%#{term.to_s.downcase.gsub(/\s+/,'')}%", "%#{term.to_s.downcase}%")
+    .unenrolled(user)
   }
 
   def schedulable?
