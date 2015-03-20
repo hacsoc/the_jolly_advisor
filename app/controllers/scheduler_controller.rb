a6e2c1f0dbf3e084ec99a8cdf836c83dda03ca4d
Andrew Mason <mason@case.edu>
2015-03-20 02:01:08 -0400

    timecop magic to allow viewing of different semesters

--- app/controllers/scheduler_controller.rb
+++ app/controllers/scheduler_controller.rb
@@ -4,6 +4,7 @@ class SchedulerController < ApplicationController
 
   # GET /scheduler
   # GET /scheduler.json
+  # GET /scheduler.js
   def index
   end
 
@@ -12,11 +13,35 @@ class SchedulerController < ApplicationController
   # Set the course instances to be rendered in the schedule
   # This is for the JSON feed that fullcalendar requires
   def set_scheduled_meetings
-    @scheduled_meetings = current_user.enrolled_courses.ongoing.includes(:meetings).flat_map(&:meetings).flat_map(&:scheduled_meetings)
+    if semester_request?
+      set_timecop
+    end
+    # timecop should be set here to get the correct enrollments in the query
+    meetings = current_user.enrolled_courses.ongoing.includes(:meetings, :course).flat_map(&:meetings)
+    if semester_request?
+      reset_timecop
+    end
+    # But timecop needs to be unset here to make sure the dates of the scheduled_meetings
+    # are actually in the current week, which is what fullcalendar is expecting
+    @scheduled_meetings = meetings.flat_map(&:scheduled_meetings)
+  end
+
+  def set_timecop
+    jump_date_string = Semester::SAFE_JUMP_DATES[params[:semester][:semester].to_sym][params[:semester][:half].to_sym] + " #{params[:semester][:year]}"
+    jump_date = DateTime.strptime(jump_date_string, Semester::SAFE_JUMP_DATE_STRPTIME_STRING + ' %Y')
+    Timecop.travel(jump_date)
+  end
+
+  def reset_timecop
+    Timecop.return
   end
 
   # move this to application controller eventually
   def json_request?
     request.format.json?
+  end
+
+  def semester_request?
+    params.include? :semester
   end
 end
--- app/models/semester.rb
+++ app/models/semester.rb
@@ -1,4 +1,33 @@
 class Semester < ActiveRecord::Base
+  # This hash is to be used in the SchedulerController
+  # It contains dates that are "guaranteed" to always
+  # be within the given semester. There are dates for
+  # both the first and second halves of semesters to
+  # account for the half-semester gym classes.
+  # When looking to show a schedule for a particular
+  # semester, the scheduler can jump to one of these
+  # dates. Just append the desired year, and use
+  # the SAFE_JUMP_DATE_STRPTIME_STRING to get the DateTime
+  # object (use DateTime so the timezone is consistent).
+  SAFE_JUMP_DATES = {
+    fall: {
+      first: 'September 30',
+      second: 'November 30'
+    },
+    spring: {
+      first: 'February 01',
+      second: 'April 01'
+    },
+    summer: {
+      # TODO: uhhhhh
+    }
+  }
+
+  # Append to this with whatever format is approprite
+  # for how you are specifying the year. Then call
+  # DateTime.strptime on that.
+  SAFE_JUMP_DATE_STRPTIME_STRING = '%B %d'
+
   def to_s
     "#{semester} #{year}"
   end
