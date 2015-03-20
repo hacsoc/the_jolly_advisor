6f1845c448b727258183e3ded30420f546e7d720
Andrew Mason <mason@case.edu>
2015-03-20 12:05:52 -0400

    update the autocomplete source on calendar reload. also use some helpers

--- app/helpers/scheduler_helper.rb
+++ app/helpers/scheduler_helper.rb
@@ -2,4 +2,9 @@ module SchedulerHelper
   def scheduler_feed
     scheduler_path(semester: params[:semester] || nil)
   end
+
+  def course_instance_autocomplete(jump_date = Date.today)
+    # jump_date could be a DateTime, so just make sure it's actually a Date
+    autocomplete_course_instances_path(current_date: jump_date.to_date)
+  end
 end
