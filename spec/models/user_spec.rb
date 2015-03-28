ea74ffea8321aef2dd5d3067b251bdfe8944648d
Andrew Mason <mason@case.edu>
2015-03-28 17:45:18 -0400

    adding shoulda-matchers with an example

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should have_many :wishlist }
+  it { should have_many :enrollments }
+  it { should have_many :enrolled_courses }
 end
