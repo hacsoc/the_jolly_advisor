cba8de30a6137de747751e2626633d3c56f19304
Andrew Mason <mason@case.edu>
2015-04-14 15:48:59 -0400

    add start and end dates to course instances

--- spec/factories/course_instances.rb
+++ spec/factories/course_instances.rb
@@ -1,5 +1,8 @@
 FactoryGirl.define do
   factory :course_instance do
+    start_date { Date.today.beginning_of_week }
+    end_date { Date.today.end_of_week }
+
     association :professor
   end
 end
