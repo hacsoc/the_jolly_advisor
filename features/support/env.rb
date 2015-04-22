7acedafc7b141338d9e93e12463b214b0f54a3ee
Andrew Mason <mason@case.edu>
2015-04-21 21:39:41 -0400

    use wear_merged to merge spec/feature coverage.
    also, always pass the 'rails' option for optimum
    railsiness

--- features/support/env.rb
+++ features/support/env.rb
@@ -4,7 +4,7 @@
 # instead of editing this one. Cucumber will automatically load all features/**/*.rb
 # files.
 require 'coveralls'
-Coveralls.wear!
+Coveralls.wear_merged!('rails')
 require 'factory_girl_rails'
 require 'cucumber/rails'
 
