267513282dd4c7bb2e8a55d06a46f79efb889717
Andrew Mason <mason@case.edu>
2015-04-24 00:25:10 -0400

    fixes twitter bullshit. closes #82.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -29,7 +29,9 @@ Rails.application.configure do
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
+  # false is the default. Setting this to true fixes the twitter bootstrap
+  # glyphicons 404 error.
+  config.assets.compile = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
