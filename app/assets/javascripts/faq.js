d818e5581fa1181b07a84bd2116134bfb8bdf472
bhc24 <bhc24@case.edu>
2015-04-28 00:06:36 -0700

    Made FAQ page look pretty

--- app/assets/javascripts/faq.js
+++ app/assets/javascripts/faq.js
@@ -1,18 +1,18 @@
 
 // Show/hide answers to questions
 jQuery(function ($) {
-    $('.panel-heading span.clickable').on("click", function (e) {
+    $('.panel-heading span.clickable').on('click', function () {
         if ($(this).hasClass('panel-collapsed')) {
             // expand the panel
             $(this).parents('.panel').find('.answer-body').slideDown();
             $(this).removeClass('panel-collapsed');
-            $(this).find('span').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
+            $(this).find('i').removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
         }
         else {
             // collapse the panel
             $(this).parents('.panel').find('.answer-body').slideUp();
             $(this).addClass('panel-collapsed');
-            $(this).find('span').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
+            $(this).find('i').removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
         }
     });
 });
