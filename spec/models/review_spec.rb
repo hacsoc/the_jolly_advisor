c6983f1388e20f513beff087ae5b9d4eac1c4409
Andrew Mason <mason@case.edu>
2015-05-02 12:46:32 -0400

    adding specs

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,4 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
+  it { should belong_to :course }
+  it { should belong_to :professor }
+  it { should belong_to :user }
+
+  let(:review) { FactoryGirl.create(:review) }
+
+  describe '#upvote' do
+    it 'should increase helpfulness by 1' do
+      helpfulness = review.helpfulness
+      review.upvote
+      expect(review.helpfulness).to eq (helpfulness + 1)
+    end
+
+    it 'should persist the helpfulness to the database' do
+      helpfulness = review.helpfulness
+      review.upvote
+      expect(review.reload.helpfulness).to eq (helpfulness + 1)
+    end
+  end
+
+  describe '#downvote' do
+    it 'should decrease helpfulness by 1' do
+      helpfulness = review.helpfulness
+      review.downvote
+      expect(review.helpfulness).to eq (helpfulness - 1)
+    end
+
+    it 'should persist the helpfulness to the database' do
+      helpfulness = review.helpfulness
+      review.downvote
+      expect(review.reload.helpfulness).to eq (helpfulness - 1)
+    end
+  end
 end
