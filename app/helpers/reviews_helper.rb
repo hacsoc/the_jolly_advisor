c8ac032fa1cabdb8c9de3252be203c6feb19b4d0
Andrew Mason <mason@case.edu>
2015-05-12 15:00:45 -0400

    add helper method for disabling voting links

--- app/helpers/reviews_helper.rb
+++ app/helpers/reviews_helper.rb
@@ -1,2 +1,5 @@
 module ReviewsHelper
+  def disable_vote_link?(review, user, score)
+    review.votes.any? { |v| v.user == user && v.score == score }
+  end
 end
--- spec/helpers/reviews_helper_spec.rb
+++ spec/helpers/reviews_helper_spec.rb
@@ -1,4 +1,40 @@
 require 'rails_helper'
 
 RSpec.describe ReviewsHelper, type: :helper do
+  describe '#disable_vote_link?' do
+    let(:review) { FactoryGirl.create(:review) }
+    let(:user) { FactoryGirl.create(:user) }
+    before { FactoryGirl.create_list(:review_vote, 2, review: review) }
+
+    context 'when the user has not voted for the review' do
+      it 'returns false' do
+        expect(helper.disable_vote_link?(review, user, 1)).to be false
+        expect(helper.disable_vote_link?(review, user, -1)).to be false
+      end
+    end
+
+    context 'when the user has voted for the review with the same score' do
+      it 'returns true for upvotes' do
+        FactoryGirl.create(:review_vote, review: review, user: user, score: 1)
+        expect(helper.disable_vote_link?(review, user, 1)).to be true
+      end
+
+      it 'returns true for downvotes' do
+        FactoryGirl.create(:review_vote, review: review, user: user, score: -1)
+        expect(helper.disable_vote_link?(review, user, -1)).to be true
+      end
+    end
+
+    context 'when the user has voted for the review with a different score' do
+      it 'returns false for upvotes' do
+        FactoryGirl.create(:review_vote, review: review, user: user, score: -1)
+        expect(helper.disable_vote_link?(review, user, 1)).to be false
+      end
+
+      it 'returns false for downvotes' do
+        FactoryGirl.create(:review_vote, review: review, user: user, score: 1)
+        expect(helper.disable_vote_link?(review, user, -1)).to be false
+      end
+    end
+  end
 end
