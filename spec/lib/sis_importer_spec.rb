6e80cc27de7051cfbdaee105ae82e6aeb5dd5720
Andrew Mason <mason@case.edu>
2015-08-25 12:23:33 -0400

    more specs. give up on not requiring all of rails

--- spec/lib/sis_importer_spec.rb
+++ spec/lib/sis_importer_spec.rb
@@ -1,9 +1,4 @@
-require 'lib_helper'
-
-# SISImporter requires these implicitly through the Rails env, but we're being
-# specific here so we don't have to load all of rails.
-require_relative '../../config/initializers/patches/array'
-require 'active_record'
+require 'rails_helper'
 
 require 'sis_importer'
 
@@ -15,6 +10,62 @@ RSpec.describe SISImporter do
       term_info = SISImporter.fetch_term_info xml
       expect(term_info.semester).to eq 'Fall'
       expect(term_info.year).to eq '2015'
+    end
+  end
+
+  describe '::process_term' do
+    let(:xml) { double(xpath: []) }
+    let(:term_info) { double(semester: 'Fall', year: '2015') }
+
+    before do
+      Semester.destroy_all(
+        semester: term_info.semester,
+        year: term_info.year
+      )
+    end
+
+    it 'creates a new semester if needed' do
+      expect do
+        SISImporter.process_term(xml, term_info)
+      end.to change { Semester.count }.by 1
+    end
+
+    it 'does not create duplicate semesters' do
+      Semester.create(
+        semester: term_info.semester,
+        year: term_info.year
+      )
+      expect do
+        SISImporter.process_term(xml, term_info)
+      end.to_not change { Semester.count }
+    end
+  end
+
+  describe '::fetch_title_and_subtitle' do
+    before { @result = SISImporter.fetch_title_and_subtitle(full_title) }
+
+    context 'for special topics courses' do
+      let(:full_title) { 'Special Topics: Name of Course' }
+
+      it "sets the title to 'Special Topics'" do
+        expect(@result[:title]).to eq 'Special Topics'
+      end
+
+      it 'sets the subtitle to what comes after the colon' do
+        expect(@result[:subtitle]).to eq 'Name of Course'
+      end
+    end
+
+    context 'for regular courses' do
+      let(:full_title) { 'Regular Course' }
+
+      it 'does not return a subtitle' do
+        expect(@result[:subtitle]).to be nil
+      end
+
+      it 'returns the full title' do
+        expect(@result[:title]).to eq full_title
+      end
     end
   end
 end
