69214e8d320bfefaccb968be06cdd228654f9327
Andrew Mason <mason@case.edu>
2015-10-08 04:15:18 -0400

    quick string formatting thing

--- spec/models/course_spec.rb
+++ spec/models/course_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe Course, type: :model do
     @course_bad = FactoryGirl.build(:course, department: 132, course_number: "EECS")
   end
 
-  describe ".postrequisites" do
+  describe "#postrequisites" do
     it "should return all postrequisites for the course" do
       prereq = FactoryGirl.create(:course, department: "EECS", course_number: 233)
       postreq = FactoryGirl.create(:course, department: "EECS", course_number: 131)
@@ -21,7 +21,7 @@ RSpec.describe Course, type: :model do
     end
   end
 
-  describe ".prerequisites" do
+  describe "#prerequisites" do
     it "should return all prerequisites for the course" do
       postreq = FactoryGirl.create(:course, department: "EECS", course_number: 131)
       prereqs = FactoryGirl.create_list(:course, 3)
@@ -83,7 +83,7 @@ RSpec.describe Course, type: :model do
     end
   end
 
-  describe ".schedulable?" do
+  describe "#schedulable?" do
     it "should say the class is schedulable" do
       expect(@course.schedulable?).to be true
     end
@@ -115,19 +115,19 @@ RSpec.describe Course, type: :model do
     end
   end
 
-  describe ".to_param" do
+  describe "#to_param" do
     it "should return a spaceless version of to_s" do
       expect(@course.to_param).to eq @course.to_s.gsub(' ', '')
     end
   end
 
-  describe ".to_s" do
+  describe "#to_s" do
     it "should return a string of the department and the course number" do
       expect(@course.to_s).to eq "EECS 132"
     end
   end
 
-  describe ".long_string" do
+  describe "#long_string" do
     it "should return a long description" do
       expect(@course.long_string).to match /[A-Z]{4}\s[\d]{3,4}:\s/
     end
