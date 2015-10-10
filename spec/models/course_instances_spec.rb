80272f05a079e7cf0110bf6fb09545112dca50a6
Andrew Mason <mason@case.edu>
2015-10-09 22:49:08 -0400

    more stubbing, less FG, more speed

--- spec/models/course_instances_spec.rb
+++ spec/models/course_instances_spec.rb
@@ -9,41 +9,44 @@ RSpec.describe CourseInstance, type: :model do
   it { should have_many :enrollments }
   it { should have_many(:enrolled_students).through(:enrollments) }
 
-  before do
-    @course_instance_yesterday = FactoryGirl.build(:course_instance, end_date: Date.today - 1)
-    @course_instance_today = FactoryGirl.build(:course_instance, end_date: Date.today)
-    @course_instance_tomorrow = FactoryGirl.build(:course_instance, end_date: Date.today + 1)
-  end
+  describe '#schedulable?' do
+    before(:all) { @course_instance = CourseInstance.new }
 
-  describe ".schedulable?" do
-    it "should return true if the class hasn't ended yet" do
-      expect(@course_instance_tomorrow.schedulable?).to be true
+    it "returns true if the class hasn't ended yet" do
+      @course_instance.end_date = Date.today + 1
+      expect(@course_instance.schedulable?).to be true
     end
 
-    it "should return false if the class has ended" do
-      expect(@course_instance_yesterday.schedulable?).to be false
+    it "returns false if the class has ended" do
+      @course_instance.end_date = Date.today - 1
+      expect(@course_instance.schedulable?).to be false
     end
 
-    it "should return false if the class ends today" do
-      expect(@course_instance_today.schedulable?).to be false
+    it "returns false if the class ends today" do
+      @course_instance.end_date = Date.today
+      expect(@course_instance.schedulable?).to be false
     end
   end
 
   describe '#schedule' do
+    before do
+      @course_instance = CourseInstance.new
+      allow(@course_instance).to receive(:meetings).and_return meetings
+    end
+
     context 'when there are no meetings' do
-      let(:course_instance) { FactoryGirl.build(:course_instance) }
+      let(:meetings) { [] }
 
       it 'returns "TBA"' do
-        expect(course_instance.schedule).to eq 'TBA'
+        expect(@course_instance.schedule).to eq 'TBA'
       end
     end
 
     context 'when there are meetings' do
-      let(:meetings) { FactoryGirl.build_list(:meeting, 2, schedule: 'MW 10:00 AM - 10:50 AM') }
-      let(:course_instance) { FactoryGirl.build(:course_instance, meetings: meetings) }
+      let(:meetings) { [double(schedule: 'MW 10:00 AM - 10:50 AM')] * 2 }
 
       it 'returns the meeting schedules separated by a ;' do
-        expect(course_instance.schedule).to eq 'MW 10:00 AM - 10:50 AM; MW 10:00 AM - 10:50 AM'
+        expect(@course_instance.schedule).to eq 'MW 10:00 AM - 10:50 AM; MW 10:00 AM - 10:50 AM'
       end
     end
   end
