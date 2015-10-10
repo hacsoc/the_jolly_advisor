45c76deb2b0799e8c56edf7ff9674283ca57bdfe
Andrew Mason <mason@case.edu>
2015-10-10 13:06:34 -0400

    add specs for show action on CoursesController

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -9,4 +9,18 @@ RSpec.describe CoursesController, type: :controller do
       expect(assigns(:semesters)).to eq semesters
     end
   end
+
+  describe 'GET show' do
+    it 'does a 404 when the course cannot be found' do
+      allow(Course).to receive(:find_by).and_return(nil)
+      expect { get :show, {id: 'EECS395'} }.to raise_error(ActionController::RoutingError)
+    end
+
+    it 'sets the course in the viaw' do
+      course = spy
+      allow(Course).to receive(:find_by).and_return(course)
+      get :show, {id: 'EECS395'}
+      expect(assigns(:course)).to eq course
+    end
+  end
 end
