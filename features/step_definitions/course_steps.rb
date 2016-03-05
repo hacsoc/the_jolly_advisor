e3fadca56727518988b4946722a1a043196d7b09
Andrew Mason <andrew@fixedpoint.xyz>
2016-03-05 11:10:46 -0500

    make course viewing step more dynamic

--- features/step_definitions/course_steps.rb
+++ features/step_definitions/course_steps.rb
@@ -1,4 +1,11 @@
-When(/^I am viewing a course$/) do
-  @course = Course.first || FactoryGirl.create(:course)
+When(/^I am viewing (a|that) course$/) do |selection|
+  @course =
+    case selection
+    when 'a'
+      Course.first || FactoryGirl.create(:course)
+    when 'that'
+      @course
+    end
+  expect(@course).to_not be nil
   step "I visit \"/courses/#{@course.to_param}\""
 end
