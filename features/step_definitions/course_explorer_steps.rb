1c8908bfa177e45538bcbfc6dac79408675cd540
Andrew Mason <andrew@fixedpoint.xyz>
2017-10-18 21:30:31 -0400

    Prettier multiline method calls in feature steps

--- features/step_definitions/course_explorer_steps.rb
+++ features/step_definitions/course_explorer_steps.rb
@@ -6,8 +6,12 @@ end
 
 Given(/^The course ([A-Z]+) (\d+) exists$/) do |department, course_number|
   Course.exists?(department: department, course_number: course_number) ||
-    FactoryGirl.create(:course, :with_course_instance,
-                       department: department, course_number: course_number)
+    FactoryGirl.create(
+      :course,
+      :with_course_instance,
+      department: department,
+      course_number: course_number,
+    )
 end
 
 When(/^I search for a professor$/) do
