9f2927418709dee09190b8cf2a980ab3fe84540d
Andrew Mason <andrew@fixedpoint.xyz>
2017-10-20 16:43:18 -0400

    Add spec for jbuilder course#show view

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require 'factory_girl_rails'
 
 RSpec.describe CoursesController, type: :controller do
   describe 'GET index' do
@@ -22,6 +23,26 @@ RSpec.describe CoursesController, type: :controller do
       allow(Course).to receive(:find_by).and_return(course)
       get :show, params: {id: 'EECS395'}
       expect(assigns(:course)).to eq course
+    end
+
+    context '.json' do
+      render_views
+
+      it 'renders' do
+        course = FactoryGirl.create(:course)
+        get :show, params: {id: course.to_param}, format: :json
+        expected_keys = [
+          'id',
+          'department',
+          'course_number',
+          'description',
+          'course_offering',
+          'created_at',
+          'updated_at',
+        ].sort
+        expect(response.success?).to be true
+        expect(JSON.parse(response.body).keys.sort).to eq expected_keys
+      end
     end
   end
 end
