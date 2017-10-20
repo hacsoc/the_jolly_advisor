10a901ee8be0533ee368c3fb1e068155384937b7
Andrew Mason <andrew@fixedpoint.xyz>
2017-10-20 10:44:29 -0400

    Disable MethodName cop for TBA method
    Rubocop does not realize this method name is an acronym. Instead it
    warns that it should be snake_case.

--- app/models/professor.rb
+++ app/models/professor.rb
@@ -1,8 +1,10 @@
 class Professor < ActiveRecord::Base
-  class << self
-    def TBA
-      where(name: "TBA").first_or_create
-    end
+  # rubocop does not realize this is an acronym and wants it to be snake_case
+  # instead.
+  #
+  # rubocop:disable Style/MethodName
+  def self.TBA
+    where(name: "TBA").first_or_create
   end
 
   scope :order_by_realness, -> { order('CASE WHEN("name" = \'Staff\' OR "name" = \'TBA\') THEN 1 ELSE 0 END') }
